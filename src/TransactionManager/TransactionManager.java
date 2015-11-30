97d661a7b4db7a038cd4f27034ce42cf2420a73a
Monica Ung <monica.ung@mail.mcgill.ca>
2015-11-30 12:47:38 -0500

    Case 1 done. On coord recovery, send abort

--- src/TransactionManager/TransactionManager.java
+++ src/TransactionManager/TransactionManager.java
@@ -1,9 +1,17 @@
 package TransactionManager;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.Map.Entry;
+import java.util.StringTokenizer;
 import java.util.Vector;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
@@ -12,6 +20,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
 
 import server.Logger2PC;
 import server.MiddlewareImpl;
@@ -52,8 +61,64 @@ public class TransactionManager {
 		proxyRoom = room;
 		
 		this.logger = new Logger2PC(Type.coordinator);
+		
+		recover();
 	}
 	
+	private void recover() {
+		// Go through coordinator log to see if there are any unfinished 2PC 
+		try {
+			File log = new File("coordinator/log.txt");
+			if (!log.exists()) return;
+			
+			FileInputStream in = new FileInputStream(log);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				System.out.println("Read: " + line);
+				StringTokenizer tokens = new StringTokenizer(line, ",");
+				if (tokens.countTokens() == 1) {
+					int txn = Integer.parseInt(tokens.nextToken());
+					System.out.println("Log finds start of txn " + txn);
+					
+					// Read next line to see if this txn completed
+					line = br.readLine();
+					System.out.println("Next line: " + line);
+					if (line == null || (new StringTokenizer(line, ",").countTokens() != 2)) {
+						// Finish the 2PC for this transaction by sending abort 
+						System.out.println("Detect unfinished 2PC protocol. Sending abort of txn " + txn);
+						try {
+							mw.abort(txn);
+						} catch (Exception e) {
+							// May receive invalid/aborted exception since it might have timed out at RM's
+						}
+						try {
+							proxyFlight.abort(txn);
+						} catch (Exception e) {
+							// May receive invalid/aborted exception since it might have timed out at RM's
+						}
+						try {
+							proxyCar.abort(txn);
+						} catch (Exception e) {
+							// May receive invalid/aborted exception since it might have timed out at RM's
+						}
+						try {
+							proxyRoom.abort(txn);
+						} catch (Exception e) {
+							// May receive invalid/aborted exception since it might have timed out at RM's
+						}
+						
+						if (line == null) return;
+					} else {
+						continue;
+					}
+				} 
+			}
+		} catch (IOException e) {
+			System.out.println("Log file does not exist.  No coordinator recovery needed.");
+		}
+	}
 	public boolean isValidTransaction(int txnID) {
 		return activeTransactions.contains(txnID);
 	}
@@ -72,7 +137,10 @@ public class TransactionManager {
 		return counter;
 	}
 	synchronized public boolean commit(int txnID) throws InvalidTransactionException {
-		System.out.println("TM:: Committing transaction "+txnID);    
+		System.out.println("TM:: Starting 2PC for txn "+txnID);    
+		
+		System.out.println("TM:: Logging start of 2PC");
+		logger.log(Integer.toString(txnID));
 		
 		if (crashPoint == 1) mw.selfDestruct();
 		
